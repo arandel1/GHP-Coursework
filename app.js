6048cdc081a50c11b5416e021f3c1d96f96a0899
Allison Randel <allisonrandel@allisons-mbp-2.attlocal.net>
2024-01-17 15:42:12 -0600

    add block07 workshop

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 - 40 - 39.")
+
+// 1. Created this .js file ("app.js")
+// 2. Created an .html ("index.html") file and linked this .js file to it, using <script>src="app.js"></script>.
+// 3. Create an alert popup with the string: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// 4. Create a unique arithmetic operator that equals 10. Call it combo1.
+// 5. Create a unique aritmetic line that equals 40. Call it combo2.
+// 6. Create a unique arithmetic line that equals 39. Call it combo3.
+// 7. Create end alert with combination results.
+
+const combo1 = 7 + 3;
+const combo2 = 4 * 10;
+const combo3 = 50 - 11; 
+
+alert(combo1 + "-" + combo2 + "-" + combo3)
